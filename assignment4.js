94dca2b14be812bdfe9e041c0891ce2cb6ac112d
Steven R <stevenroomberg@gmail.com>
2017-04-12 15:19:40 -0400

    assignment finished

--- assignment4.js
+++ assignment4.js
@@ -12,6 +12,39 @@
 // keyup events could be helpful to get value of field as the user types
 
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+	var datatags = [
+		'all',
+		'interests',
+		'programming',
+		'comics'
+	];
+	var mbapi = 'http://www.mattbowytz.com/simple_api.json';
+
+	$('.flexsearch-input').keyup(function() {
+		var currentdata = $(this).val();
+		$.getJSON(mbapi, currentdata, function(data) {
+			// console.log(currentdata);
+			$('.search-results > ul').empty();
+
+			// special case for empty string
+			if (currentdata == '') return;
+			
+			var searchitems = [];
+			$.each(datatags, function(index) {
+				if (datatags[index].includes(currentdata)) {
+					searchitems.push(
+						'<li class="search-result-item"><a href="https://www.google.com/#q=matt+bowytz+'
+						+ datatags[index] + '" target="_blank">' + datatags[index] + '</a></li>');
+				}
+			});
+			$('.search-results > ul').append(searchitems);
+		});
+	});
+
+	$('.flexsearch-submit').on('submit', function() {
+		var currentdata = $('.flexsearch-input').val();
+		location.search = currentdata;
+		window.location.href += location.search;
+	});
+
 })();
